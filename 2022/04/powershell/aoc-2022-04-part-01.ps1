1c9fa80faaaecf27bb11cb0f371834ae8409976c
Keith <youngka@gmail.com>
2024-06-13 09:43:46 -0400

    2022 aoc day 4

---
+++ 2022/04/powershell/aoc-2022-04-part-01.ps1
@@ -0,0 +1,28 @@
+﻿$InData = Get-Content -Path C:\Temp\aoc-2022-04-puzzle-input.txt -Encoding Default
+$Ctr = 0
+# 14-28,13-28
+# 72-81,82-91
+# 18-18,18-55
+# 7-98,7-98
+# 1-20,2-19
+# 3-17,1-2
+
+ForEach($Row in $Indata)
+{
+    # $Row -match '^(\d{1,2})-(\d{1,2}),(\d{1,2})-(\d{1,2})$' | Out-Null
+    $Split = $Row -split ','
+    $Left = $Split[0] -split '-'
+    $Right = $Split[1] -split '-'
+    If(
+        ([int]$Left[0] -ge [int]$Right[0] -and [int]$Left[1] -le [int]$Right[1] ) -or
+        ([int]$Right[0] -ge [int]$Left[0] -and [int]$Right[1] -le [int]$Left[1] )
+    )
+    {
+        Write-Host "$Row"
+        $Ctr++
+    }
+}
+
+$Ctr
+
+
---
+++ 2022/04/powershell/aoc-2022-04-part-02.ps1
@@ -0,0 +1,56 @@
+﻿$InData = Get-Content -Path C:\Temp\aoc-2022-04-puzzle-input.txt -Encoding Default
+$Ctr = 0
+
+
+Function Unpack-Row
+{
+    Param(
+        [string]$Row,
+        [ref]$LeftLow,
+        [ref]$LeftHigh,
+        [ref]$RightLow,
+        [ref]$RightHigh
+    )
+    $Split = $Row -split ','
+    $Left = $Split[0] -split '-'
+    $Right = $Split[1] -split '-'
+    $LeftLow.Value = $Left[0]
+    $LeftHigh.Value = $Left[1]
+    $RightLow.Value = $Right[0]
+    $RightHigh.Value = $Right[1]
+}
+
+[int]$LeftLow = 0
+[int]$LeftHigh = 0
+[int]$RightLow = 0
+[int]$RightHigh = 0
+
+
+$TestAra = @(
+    '2-4,6-8',
+    '2-3,4-5',
+    '5-7,7-9',
+    '2-8,3-7',
+    '6-6,4-6',
+    '2-6,4-8'
+)
+
+
+ForEach($Row in $InData)
+{
+    Unpack-Row -Row $Row -LeftLow ([ref]$LeftLow) -LeftHigh ([ref]$LeftHigh) -RightLow ([ref]$RightLow) -RightHigh ([ref]$RightHigh)
+    If(
+        ($LeftLow -ge $RightLow -and $LeftLow -le $RightHigh) -or
+        ($LeftHigh -ge $RightLow -and $LeftHigh -le $RightHigh) -or
+        ($RightLow -ge $LeftLow -and $RightLow -le $LeftHigh) -or
+        ($RightHigh -ge $LeftLow -and $RightHigh -le $LeftHigh) 
+    )
+    {
+        # Write-Host ("{0}-{1},{2}-{3}" -f $LeftLow, $LeftHigh, $RightLow, $RightHigh) -BackgroundColor DarkCyan
+        $Ctr++
+    }
+}
+
+$Ctr
+
+
