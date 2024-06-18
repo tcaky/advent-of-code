4e772681a1fcebfd9ff2c23dabc8357f0896f2ee
Keith <youngka@gmail.com>
2024-06-18 18:11:09 +0000

    using a build of powershell in the devcontainer

--- .devcontainer/alpine-powershell/post-create-command.sh
+++ .devcontainer/alpine-powershell/post-create-command.sh
@@ -1,7 +1,48 @@
 #!/bin/sh
+
+# install the requirements
+sudo apk add --no-cache \
+    ca-certificates \
+    less \
+    ncurses-terminfo-base \
+    krb5-libs \
+    libgcc \
+    libintl \
+    libssl3 \
+    libstdc++ \
+    tzdata \
+    userspace-rcu \
+    zlib \
+    icu-libs \
+    curl
+
+sudo apk -X https://dl-cdn.alpinelinux.org/alpine/edge/main add --no-cache \
+    lttng-ust
+
+# Download the powershell '.tar.gz' archive
+curl -L https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/powershell-7.4.2-linux-musl-x64.tar.gz -o /tmp/powershell.tar.gz
+
+# Create the target folder where powershell will be placed
+sudo mkdir -p /opt/microsoft/powershell/7
+
+# Expand powershell to the target folder
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+
+# Set execute permissions
+sudo chmod +x /opt/microsoft/powershell/7/pwsh
+
+# Create the symbolic link that points to pwsh
+sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+
+# Start PowerShell
+# pwsh
+
+
+
+
 echo "Ensure pwsh is in /etc/shells"
 PWSH_PATH=$(sudo which pwsh)
 echo "PWSH_PATH=$PWSH_PATH"
 grep -x $PWSH_PATH /etc/shells || sudo sh -c "echo $PWSH_PATH >> /etc/shells"
 echo "switch shell to pwsh"
-sudo chsh vscode -s $PWSH_PATH
+# sudo chsh vscode -s $PWSH_PATH
