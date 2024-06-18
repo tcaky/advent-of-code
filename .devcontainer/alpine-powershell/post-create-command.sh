7f9fe1f37ba24432bfd871623d464cce152b2414
Keith <youngka@gmail.com>
2024-06-18 16:31:41 +0000

    added execute bit to shell script

--- .devcontainer/alpine-powershell/post-create-command.sh
+++ .devcontainer/alpine-powershell/post-create-command.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-
-sudo chsh vscode -s \"$(which pwsh)\"
+echo "Ensure pwsh is in /etc/shells"
+PWSH_PATH=$(sudo which pwsh)
+echo "PWSH_PATH=$PWSH_PATH"
+grep -x $PWSH_PATH /etc/shells || sudo sh -c "echo $PWSH_PATH >> /etc/shells"
+echo "switch shell to pwsh"
+sudo chsh vscode -s $PWSH_PATH
